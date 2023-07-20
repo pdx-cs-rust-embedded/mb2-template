bad56b762fbe1315445d03cf3089b7c3efd0c85c
Bart Massey <bart.massey@gmail.com>
2023-07-19 18:36:36 -0700

    built speaker output demo

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 #![no_main]
 #![no_std]
 
-use rtt_target::{rtt_init_print, rprintln};                                   
-use panic_rtt_target as _;                                                    
+use panic_halt as _;
 
 use cortex_m_rt::entry;
-use microbit::board::Board;
+use microbit::Board;
+use microbit::hal::{prelude::*, delay::Delay, gpio::Level};
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
     let board = Board::take().unwrap();
+    let mut delay = Delay::new(board.SYST);
+    let mut speaker = board.speaker_pin.into_push_pull_output(Level::Low);
     loop {
+        speaker.set_high().unwrap();
+        delay.delay_us(500u16);
+        speaker.set_low().unwrap();
+        delay.delay_us(500u16);
     }
 }
