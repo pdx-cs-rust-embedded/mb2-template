dad8fcbf960170609e90bb67510d5aa85453dbce
Bart Massey <bart.massey@gmail.com>
2023-07-19 17:54:10 -0700

    MB2 project template

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# gpio-hal-printbuttons: button interrupt handler demo
-
-Demonstrate the setup and use of a button interrupt, using
-RTT tracing prints.
-
-Note: This example is taken from the `microbit` crate
-source, and lightly modified to work.
+# mb2-template: MicroBit 2 Rust embedded project template
--- src/main.rs
+++ src/main.rs
@@ -4,85 +4,13 @@
 use rtt_target::{rtt_init_print, rprintln};                                   
 use panic_rtt_target as _;                                                    
 
-use core::cell::RefCell;
-
-use cortex_m::{
-    asm,
-    interrupt::Mutex,
-};
 use cortex_m_rt::entry;
-use microbit::{
-    board::Board,
-    hal::gpiote::Gpiote,
-    pac::{self, interrupt},
-};
-
-static GPIO: Mutex<RefCell<Option<Gpiote>>> = Mutex::new(RefCell::new(None));
+use microbit::board::Board;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = Board::take().unwrap();
-
-    let gpiote = Gpiote::new(board.GPIOTE);
-
-    let channel0 = gpiote.channel0();
-    channel0
-        .input_pin(&board.buttons.button_a.degrade())
-        .hi_to_lo()
-        .enable_interrupt();
-    channel0.reset_events();
-
-    let channel1 = gpiote.channel1();
-    channel1
-        .input_pin(&board.buttons.button_b.degrade())
-        .hi_to_lo()
-        .enable_interrupt();
-    channel1.reset_events();
-
-    cortex_m::interrupt::free(move |cs| {
-        /* Enable external GPIO interrupts */
-        unsafe {
-            pac::NVIC::unmask(pac::Interrupt::GPIOTE);
-        }
-        pac::NVIC::unpend(pac::Interrupt::GPIOTE);
-
-        *GPIO.borrow(cs).borrow_mut() = Some(gpiote);
-
-        rprintln!("Welcome to the buttons demo. Press buttons A and/or B for some action.");
-    });
-
     loop {
-        asm::wfi();
-        rprintln!("got interrupt");
     }
-}
-
-// Define an interrupt, i.e. function to call when exception occurs. Here if we receive an
-// interrupt from a button press, the function will be called
-#[interrupt]
-fn GPIOTE() {
-    /* Enter critical section */
-    cortex_m::interrupt::free(|cs| {
-        rprintln!("interrupt");
-        if let Some(gpiote) = GPIO.borrow(cs).borrow().as_ref() {
-            let buttonapressed = gpiote.channel0().is_event_triggered();
-            let buttonbpressed = gpiote.channel1().is_event_triggered();
-
-            /* Print buttons to the serial console */
-            rprintln!(
-                "button pressed {:?}",
-                match (buttonapressed, buttonbpressed) {
-                    (false, false) => "",
-                    (true, false) => "A",
-                    (false, true) => "B",
-                    (true, true) => "A + B",
-                }
-            );
-
-            /* Clear events */
-            gpiote.channel0().reset_events();
-            gpiote.channel1().reset_events();
-        }
-    });
 }
